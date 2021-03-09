2fbb0c04bdc08562d45bcdb7784c048f9af485d3
RyuBAI <me@ryubai.com>
2021-03-09 16:16:18 +0800

    ...

--- include/arm7tdmi_gba_debug.h
+++ include/arm7tdmi_gba_debug.h
@@ -7,8 +7,8 @@
 
 class BusBindGBA : public BusInterface {
 public:
-    std::vector<std::tuple<std::uint32_t, std::uint32_t, std::uint8_t>>* wa;
-    BusBindGBA(std::vector<std::tuple<std::uint32_t, std::uint32_t, std::uint8_t>>* wa) : wa(wa){}
+    std::vector<std::tuple<std::uint32_t, std::uint8_t>>* wa;
+    BusBindGBA(std::vector<std::tuple<std::uint32_t, std::uint8_t>>* wa) : wa(wa){}
     std::uint8_t  R8   (std::uint32_t addr, bool isSeq){
         return *reinterpret_cast<std::uint8_t*>(&(reinterpret_cast<std::uint8_t*>(0))[addr]);
     }
@@ -19,13 +19,17 @@ public:
         return *reinterpret_cast<std::uint32_t*>(&(reinterpret_cast<std::uint8_t*>(0))[addr]);
     }
     void          W8   (std::uint32_t addr, std::uint8_t  val, bool isSeq){
-        wa->push_back(std::make_tuple(addr, val, 8));
+        wa->push_back(std::make_tuple(addr + 0, val));
     }
     void          W16  (std::uint32_t addr, std::uint16_t val, bool isSeq){
-        wa->push_back(std::make_tuple(addr, val, 16));
+        wa->push_back(std::make_tuple(addr + 0, static_cast<std::uint8_t>(val >> 0)));
+        wa->push_back(std::make_tuple(addr + 1, static_cast<std::uint8_t>(val >> 8)));
     }
     void          W32  (std::uint32_t addr, std::uint32_t val, bool isSeq){
-        wa->push_back(std::make_tuple(addr, val, 32));
+        wa->push_back(std::make_tuple(addr + 0, static_cast<std::uint8_t>(val >>  0)));
+        wa->push_back(std::make_tuple(addr + 1, static_cast<std::uint8_t>(val >>  8)));
+        wa->push_back(std::make_tuple(addr + 2, static_cast<std::uint8_t>(val >> 16)));
+        wa->push_back(std::make_tuple(addr + 3, static_cast<std::uint8_t>(val >> 24)));
     }
 };
 
@@ -33,6 +37,6 @@ class ARM7TDMI_DEBUG_GBA {
 public:
     BusBindGBA main_bus;
     ARM7TDMI cpu;
-    std::vector<std::tuple<std::uint32_t, std::uint32_t, std::uint8_t>> writeAction;
+    std::vector<std::tuple<std::uint32_t, std::uint8_t>> writeAction;
     ARM7TDMI_DEBUG_GBA() : main_bus(&writeAction), cpu(&main_bus) {}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,36 @@
 #include <gba_interrupt.h>
 #include <gba_systemcalls.h>
 #include <gba_timers.h>
+
 #include <stdio.h>
 #include <string.h>
 
 #include "arm7tdmi_gba_debug.h"
 
-void PrintAddr(){
-    
+void PrintMem(ARM7TDMI_DEBUG_GBA& sim){
+    printf("  MEMORY  | SIMU | PHYS |\n");
+    if(sim.writeAction.size() == 0) {
+        printf(" -------- |  --  |  --  |\n");
+    }else{
+        for(const auto& ele : sim.writeAction){
+            std::uint32_t addr = std::get<0>(ele);
+            std::uint8_t sim_val = std::get<1>(ele);
+            std::uint8_t phy_val = (reinterpret_cast<std::uint8_t*>(0))[addr];
+            printf(" %08lX |  %02X  |  %02X  |\n", addr, sim_val, phy_val);
+        }
+    }
 }
-bool CompareAddr(){
-
+bool VerifyMem(ARM7TDMI_DEBUG_GBA& sim){
+    if(sim.writeAction.size() == 0) return true;
+    if(sim.writeAction.size() > 4) return false;
+    for(const auto& ele : sim.writeAction)
+        if((reinterpret_cast<std::uint8_t*>(0))[std::get<0>(ele)] != std::get<1>(ele))
+            return false;
+    return true;
 }
 
 void PrintRegs(std::uint32_t* ptr_simgreg, std::uint32_t* ptr_simcpsr, std::uint32_t* ptr_phytmp){
-    printf("     | SIMULATE | PHYSICAL |\n");
+    printf(" REG | SIMULATE | PHYSICAL |\n");
     for(std::size_t i=0; i<17; i++){
 		if(i != 16)
             printf(" r%02d | %08lX | %08lX |\n", i, ptr_simgreg[i], ptr_phytmp[i]);
@@ -26,7 +42,7 @@ void PrintRegs(std::uint32_t* ptr_simgreg, std::uint32_t* ptr_simcpsr, std::uint
 	}
 }
 
-bool CompareRegs(std::uint32_t* ptr_simgreg, std::uint32_t* ptr_simcpsr, std::uint32_t* ptr_phytmp){
+bool VerifyRegs(std::uint32_t* ptr_simgreg, std::uint32_t* ptr_simcpsr, std::uint32_t* ptr_phytmp){
     bool identical = true;
     for(std::size_t i=0; i<17; i++){
 	    if(i != 16) identical = ptr_simgreg[i] == ptr_phytmp[i];
@@ -37,18 +53,26 @@ bool CompareRegs(std::uint32_t* ptr_simgreg, std::uint32_t* ptr_simcpsr, std::ui
 }
 
 __attribute__((section(".iwram.testinst")))
-extern void TestInstruction(std::uint32_t* ptr_simgreg, std::uint32_t* ptr_simcpsr, std::uint32_t* ptr_phytmp, std::uint32_t* ptr_instruction){
+extern void TestInstruction(
+    std::uint32_t* ptr_simgreg, 
+    std::uint32_t* ptr_simcpsr, 
+    std::uint32_t* ptr_phytmp, 
+    std::uint32_t* ptr_instruction,
+    std::uint32_t* ptr_wrimem){
     
     //Write test instrucion
     __asm("ldr r0, %[val]" : : [val] "m" (ptr_instruction));
     __asm("ldr r0, [r0]");
-    __asm("str r0, [pc, #28]"); //It overwrites offset + 4*(9-2), which is the test case's position
+    __asm("str r0, [pc, #32]"); //It overwrites offset + 4*(10-2), which is the test case's position
 
     //Sync cpsr of the simulator with physical processor
     __asm("ldr r0, %[val]" : : [val] "m" (ptr_simcpsr));
     __asm("mrs r1, cpsr");
     __asm("str r1, [r0]");
     
+    //Use r1 as writable memory pointer
+    __asm("ldr r2, %[val]" : : [val] "m" (ptr_wrimem));
+
     //Sync general registers of the simulator with physical processor
     __asm("ldr r0, %[val]" : : [val] "m" (ptr_simgreg));
     __asm("stmia r0, {r0-r15}");
@@ -89,21 +113,26 @@ int main(void){
 	irqEnable(IRQ_VBLANK);
 	consoleDemoInit();
 
-    //mov r2, #0xEE
-    std::uint32_t test_case[] = { 0xE3A020EE };
+    
     ARM7TDMI_DEBUG_GBA *sim = new ARM7TDMI_DEBUG_GBA();
     std::uint32_t phy_tmpreg[17] = {0};
+    std::uint32_t writable_mem[4] = {0};
+    std::uint32_t test_case[] = { 0xE3A020EE, 0xE5822000 }; //mov r2, #0xEE / str r2, [r2]
 
     std::uint32_t* ptr_simgreg = &(sim->cpu.registers.current[0]);
     std::uint32_t* ptr_simcpsr = &(sim->cpu.registers.cpsr.value);
     std::uint32_t* ptr_phytmp  = &(phy_tmpreg[0]);
-    std::uint32_t* ptr_ins     = &(test_case[0]);
+    std::uint32_t* ptr_ins     = &(test_case[1]);
+    std::uint32_t* ptr_wrimem  = &(writable_mem[0]);
 
-    TestInstruction(ptr_simgreg, ptr_simcpsr, ptr_phytmp, ptr_ins);
+    TestInstruction(ptr_simgreg, ptr_simcpsr, ptr_phytmp, ptr_ins, ptr_wrimem);
     TestInstructionFinish(sim->cpu);
 
-    PrintRegs(ptr_simgreg, ptr_simcpsr, ptr_phytmp);
-    printf(CompareRegs(ptr_simgreg, ptr_simcpsr, ptr_phytmp) ? "REG IDENTICAL!\n" : "REG NOT INDENTICAL!\n");
+    //PrintRegs(ptr_simgreg, ptr_simcpsr, ptr_phytmp);
+    //printf(VerifyRegs(ptr_simgreg, ptr_simcpsr, ptr_phytmp) ? "REG IDENTICAL!\n" : "REG NOT INDENTICAL!\n");
+
+    PrintMem(*sim);
+    printf(VerifyMem(*sim) ? "MEM IDENTICAL!\n" : "MEM NOT INDENTICAL!\n");
 
 	while(1) VBlankIntrWait();
 }
